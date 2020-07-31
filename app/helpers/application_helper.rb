2b4853424c79a5e1416d46dea15cfafbb25ffbf3
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-07-31 10:15:55 +0900

    通知機能

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def avatar_url(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+  end
 end
