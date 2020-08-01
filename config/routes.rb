ca6efa2a433c4d299f4a5374aebc2b0757e33a1f
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-08-01 22:03:57 +0900

    profile page create

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'pages/home'
-  devise_for :users
-  root 'high_scores#index'
+  root 'pages#home'
+  devise_for :users,
+             path: '',
+             path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration' }
+  get 'high_scores/index'
+  resources :users, only: [:show]
 
   resources :high_scores
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
