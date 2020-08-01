50d370db16398a199e226bca2c5b7bf827aeac87
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-08-01 22:40:24 +0900

    edit profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users,
              path: '',
-             path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration' }
+             path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration' },
+             controllers: { registrations: 'registrations' }
+
   get 'high_scores/index'
   resources :users, only: [:show]
 
